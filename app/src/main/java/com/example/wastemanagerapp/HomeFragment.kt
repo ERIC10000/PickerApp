997131336602b1b8c6c0f23d9d89cd29048b153b
ERIC10000 <erickolattiz@gmail.com>
2023-10-17 00:30:55 -0700

    Pdf Download

--- app/src/main/java/com/example/wastemanagerapp/HomeFragment.kt
+++ app/src/main/java/com/example/wastemanagerapp/HomeFragment.kt
@@ -1,17 +1,37 @@
 package com.example.wastemanagerapp
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+
 import android.os.Bundle
+import android.os.Environment
+import android.provider.DocumentsContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.AppCompatButton
+import androidx.cardview.widget.CardView
 import androidx.drawerlayout.widget.DrawerLayout
 import com.bumptech.glide.Glide
 import com.example.wastemanagerapp.helpers.PrefsHelper
+import com.itextpdf.io.image.ImageDataFactory
+import com.itextpdf.kernel.pdf.PdfDocument
+import com.itextpdf.kernel.pdf.PdfName.Document
+import com.itextpdf.kernel.pdf.PdfWriter
+import com.itextpdf.layout.Document
+
+import com.itextpdf.layout.element.Image
+
+import java.io.ByteArrayOutputStream
+import java.io.File
 
 
 class HomeFragment : Fragment() {
@@ -28,12 +48,6 @@ class HomeFragment : Fragment() {
 
 
         val root =  inflater.inflate(R.layout.fragment_home, container, false)
-
-
-
-
-
-
         val image1 : ImageView = root.findViewById(R.id.smallImage)
         val image2 : ImageView = root.findViewById(R.id.bigImage)
 
@@ -79,10 +93,53 @@ class HomeFragment : Fragment() {
         val phone = PrefsHelper.getPrefs(requireContext(), "phone")
 
         telephoneNo.text = " +254$phone"
+
+        //id  card to pdf
+        val download: AppCompatButton = root.findViewById(R.id.btn_login)
+        download.setOnClickListener {
+                val progressBar: ProgressBar = root.findViewById(R.id.progressBar)
+                progressBar.visibility = View.VISIBLE
+                val pdfFileName = "IDCARD.pdf"
+                val pdfDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+
+                val pdfFile = File(pdfDirectory, pdfFileName)
+
+                val pdfDocument = PdfDocument(PdfWriter(pdfFile))
+                val pdf = Document(pdfDocument)
+
+                // Create a bitmap from your CardView
+                val cardView : CardView = root.findViewById(R.id.cardView)
+                val cardViewBitmap = viewToBitmap(cardView)
+
+                // Convert the bitmap to an iText Image
+                val image = Image(ImageDataFactory.create(cardViewBitmapToByteArray(cardViewBitmap)))
+                pdf.add(image)
+
+                // Close the document
+                pdf.close()
+                progressBar.visibility = View.GONE
+
+
+                // Now you can save the PDF file and provide a download link or any other functionality you want.
+            Toast.makeText(requireContext(), "Saved Successfully, Proceed to Downloads", Toast.LENGTH_LONG).show()
+
+        }
+
         return  root
     }
 
+    private fun viewToBitmap(view: View): Bitmap {
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        view.layout(view.left, view.top, view.right, view.bottom)
+        view.draw(canvas)
+        return bitmap
+    }
 
-
+    private fun cardViewBitmapToByteArray(bitmap: Bitmap): ByteArray {
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        return outputStream.toByteArray()
+    }
 
 }
